8273d868ed18c72aa43e2ff947e138892e2b6366
Denys Huziuk <deniushuziuk@gmail.com>
2024-10-28 15:32:48 +0100

    Updates

--- assets/index-DPbkZUr1.js
+++ assets/index-DPbkZUr1.js
@@ -1,4 +1,4 @@
-import{r as H,g as dt}from"./index-BmNi44IM.js";var ct={exports:{}},A={exports:{}},L={exports:{}};/*!
+import{r as H,g as dt}from"./index-Be5K2f5k.js";var ct={exports:{}},A={exports:{}},L={exports:{}};/*!
  * getSize v2.0.3
  * measure size of elements
  * MIT license
